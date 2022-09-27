61a9e333ef59b5b010a64742c2e71f1f7048d861
aryanmikaeili <ar.mikaeili@yahoo.com>
2022-09-27 01:35:54 -0700

    assignment 1 - AC

---
+++ assignments/Assignment1-AC/code/internal_energy_matrix.py
@@ -0,0 +1,11 @@
+def get_matrix(alpha, beta, gamma, num_points):
+    """Return the matrix for the internal energy minimization.
+    # Arguments
+        alpha: The alpha parameter.
+        beta: The beta parameter.
+        gamma: The gamma parameter.
+        num_points: The number of points in the curve.
+    # Returns
+        The matrix for the internal energy minimization. (i.e. A + gamma * I)
+    """
+    raise NotImplementedError
